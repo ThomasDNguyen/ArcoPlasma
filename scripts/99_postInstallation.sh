a103e77023f9facfc90d968bbb2611def21a813c
Thomas <yohowdoyoudo@gmail.com>
2021-12-30 23:58:12 +0200

    Change some packages and init new zsh shell

--- scripts/99_postInstallation.sh
+++ scripts/99_postInstallation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+#: Set zsh as default shell (for $USER && root).
+chsh -s $(which zsh) && sudo chsh -s $(which zsh) && echo "#New zshrc file" > $HOME/.zshrc && zsh
+
 #: Enable essential services.
 sudo systemctl enable NetworkManager bluetooth.service sddm.service
 
-#: Set zsh as default shell (for $USER && root).
-chsh -s $(which zsh) && sudo chsh -s $(which zsh)
-
 #: Clean up files and dirs that are not needed.
-rm -rf $HOME/.screenrc $HOME/.bash* $HOME/.config/autostart/arcolinux* $HOME/.config/alacritty $HOME/.config/arcolinux-welcome-app $HOME/.config/neofetch
+rm -rf $HOME/.screenrc $HOME/.viminfo $HOME/.lesshst $HOME/.bash* $HOME/.config/autostart/arcolinux* $HOME/.config/alacritty $HOME/.config/arcolinux-welcome-app $HOME/.config/neofetch
 
 #: Remove orphan packages.
 #sudo pacman -Rn --noconfirm $(pacman -Qqdt)	# Remove true orphans.
