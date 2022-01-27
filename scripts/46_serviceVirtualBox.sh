4bd05cb782c10f475d4aac8ec80630a6ea64971b
ThomasDNguyen <yohowdoyoudo@gmail.com>
2022-01-27 11:49:42 +0200

    Misc changes.

--- scripts/46_serviceVirtualBox.sh
+++ scripts/46_serviceVirtualBox.sh
@@ -7,7 +7,7 @@
 . functions.sh
 
 #: Install packages for VirtualBox.
-sudo pacman -S --needed --noconfirm $(parse_package_list $vbox)
+paru -S --needed --noconfirm $(parse_package_list $vbox)
 
 #: Add current user to xboxusers group.
 sudo gpasswd -a $USER vboxusers
